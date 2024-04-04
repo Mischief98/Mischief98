e6360458389afa955698eef7e66a1907a2fed5ae
Mischief98 <psarathi959@gmail.com>
2024-04-04 16:55:37 +0530

    normal file

---
+++ CSS/day1_18_03_24/practice/index.js
@@ -0,0 +1,20 @@
+//console.log("allmightypush")
+//console.log(5678)
+//console.log("123+123")
+//console.log(123+123)
+//console.log("123"+"123")
+//console.log(5+"7")
+//console.log(4+5+"4"+7+8)let a=1
+{
+    let a=1
+    for (let i=0; i<=a.rows; i++) {
+        let rows='4';
+        
+    }
+    for(let r=i; r<=i; i++)
+    {
+        i<=a+rows;
+        r++;
+    }
+    console.log(rows)
+}
---
+++ CSS/day1_18_03_24/practice/personal.js
@@ -0,0 +1,87 @@
+//console.log("NAME:PARTHA SARATHI.R")
+//console.log("AGE:25")
+//console.log("COLLEGE:PARK CLG OF ENGINEERINGB AND TECHNOLOGY")
+//console.log("2020 PASSEDOUT")
+//console.log("MOBILE:7538810986")
+//let NAME=("sarathi")
+//let AGE=25
+//let PASSEDOUT=2020
+//let mobile1=5678910
+//let mobile=78902347
+//console.log(NAME)
+//console.log(AGE)
+let a=10
+let b=5
+//console.log(a>5 || b==a)
+function add()
+{
+    let a=10
+    let b=50
+    let c=a+b
+    return c
+}
+let result=add()
+//console.log(result)
+function area()
+{
+    let a=20
+    let b=20
+let total=a*b
+//console.log(total)
+}
+area()
+function area()
+{
+    let b=20
+    let h=20
+    let total=1/2*b*h
+    //console.log(total) 
+}
+area()
+function area()
+{
+    let r=20
+    let pi=3.14
+    let c=3.14*20*20
+    return c
+}
+let result=area()
+//console.log(total)
+
+
+function areaofcircle() {
+    let r=10
+    let pi=3.14
+    let circle=pi*r*r
+    return circle
+}
+call()
+console.log(answer)
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
